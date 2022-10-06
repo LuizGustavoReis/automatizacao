e205995b736cf373cb6f16f47bb0cb73315709cc
LuizGustavoReis <80647701+LuizGustavoReis@users.noreply.github.com>
2022-10-05 22:34:37 -0300

    comentando

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #from webbrowser import get
+#importando
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
@@ -7,7 +8,10 @@ import time
 
 
 navegador= webdriver.Chrome(ChromeDriverManager().install())
+#celecionando a pagina
 navegador.get('https://www.google.com.br')
+#escrever na barra de pesquisa
 navegador.find_element("xpath",'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys('cotação de dolar')
+#pesquisar dando enter
 navegador.find_element('xpath','/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
 time.sleep(100)
